5b7fee5c464aa3756cb84947c86f48c4534a071a
Abner Eduardo Silveira Santos <abner.santos@usp.br>
2018-09-21 23:40:10 -0300

    Scraped drag and drop system

---
+++ Assets/Scraped/Scripts/ModuleDrag.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleDrag : MonoBehaviour {
+
+	[SerializeField] BoxCollider2D touchCollider, hitCollider;
+	public Vector2 fixedPos;
+	public bool isHoveringSlot = false, isAttached = false;
+	public Slot attachedSlot;
+
+	private void Awake() {
+		
+	}
+
+	private void Update() {
+		if(Input.GetMouseButtonDown(0))
+		{
+			isAttached = false;
+			Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log(MousePos);
+			if(touchCollider.bounds.Contains((Vector2)MousePos))
+			{
+				Debug.Log("Attached.");
+				this.transform.SetParent(null, true);
+				InvokeRepeating("DragModule", 0, Time.deltaTime);
+			}
+		}
+	}
+
+	private void DragModule()
+	{
+		Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		this.transform.position = (Vector2)MousePos;
+
+		if(Input.GetMouseButtonUp(0) && !isHoveringSlot || isAttached || !Input.GetMouseButton(0) && !isHoveringSlot)
+		{
+			this.transform.position = (Vector2)fixedPos;
+			CancelInvoke();
+		}
+
+	}
+
+}
---
+++ Assets/Scraped/Scripts/WindowPop.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowPop : MonoBehaviour {
+
+	[SerializeField] float popUpSpeed, hidingSpeed;
+	[SerializeField] Transform hidingSpot, popUpSpot;
+	bool isHidden = true;
+
+	public void Swap()
+	{
+		if(isHidden)
+			PopUp();
+		else
+			Hide();
+	}
+
+	private void PopUp()
+	{
+		InvokeRepeating("PopUpTransition", 0, Time.deltaTime);
+	}
+
+	private void Hide()
+	{
+		InvokeRepeating("HideTransition", 0, Time.deltaTime);
+	}
+
+	private void PopUpTransition()
+	{
+		this.transform.position = Vector2.MoveTowards(this.transform.position, popUpSpot.position, popUpSpeed);
+		if(((Vector2)this.transform.position - (Vector2)popUpSpot.position).magnitude < 0f)
+		{
+			isHidden = false;
+			CancelInvoke();
+		}
+	}
+
+	private void HideTransition()
+	{
+		this.transform.position = Vector2.MoveTowards(this.transform.position, hidingSpot.position, hidingSpeed);
+		if(((Vector2)this.transform.position - (Vector2)hidingSpot.position).magnitude > 0f)
+		{
+			isHidden = true;
+			CancelInvoke();
+		}
+	}
+}
