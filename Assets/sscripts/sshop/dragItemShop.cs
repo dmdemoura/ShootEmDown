49b65094a97ed6996ee2960c9cc0f609f135cb3c
unknown <gustavosasakironcaglia@Outlook.com>
2018-09-22 00:19:15 -0300

    arrumando drag

--- Assets/sscripts/sshop/dragItemShop.cs
+++ Assets/sscripts/sshop/dragItemShop.cs
@@ -13,24 +13,34 @@ public class dragItemShop : MonoBehaviour, IDragHandler,IEndDragHandler, IDropHa
 	Vector3 posInicial;
 	private construindoNave boss;
 
+	private dropItemShop[] dpIS;
 	public void OnDrag(PointerEventData data){
 		trans.position = Input.mousePosition;
     }
     public void OnEndDrag(PointerEventData eventData){
+		
         trans.position = posInicial;
+		
+
     }
 
 	public void OnDrop(PointerEventData eventData){
-		GameObject dropped = eventData.pointerDrag;
-		Debug.Log(dropped.GetComponent<dropItemShop>());
-		RectTransform invPanel = transform as RectTransform;
-		if(RectTransformUtility.RectangleContainsScreenPoint(invPanel,Input.mousePosition)){
-			Debug.Log(dropped.name);
-			//boss.colocarArma(dropped.armaID,slotId,dropped.armaType);
+		dragItemShop dropped = eventData.pointerDrag.GetComponent<dragItemShop>();
+		for(int i=0;i<dpIS.Length;i++){
+			if(!dpIS[i].isActiveAndEnabled)
+				continue;
+			
+			RectTransform invPanel =dpIS[i].transform as RectTransform;
+			if(RectTransformUtility.RectangleContainsScreenPoint(invPanel,Input.mousePosition)){
+				Debug.Log(dropped.armaID);
+				boss.colocarArma(dropped.armaID,dpIS[i].slotId,dropped.armaType);
+			}
 		}
+
 	}
 
 	void Start(){
+		dpIS = FindObjectsOfType<dropItemShop>();
 		trans = GetComponent<Transform>();
 		posInicial = trans.position;
 		boss = FindObjectOfType<construindoNave>();
