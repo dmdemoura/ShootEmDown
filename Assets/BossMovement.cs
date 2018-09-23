763f60ced498606284ac23b629a4b91f43564d48
Gabriel <gdeperes@gmail.com>
2018-09-22 23:05:23 -0300

    Improved ccena

---
+++ Assets/BossMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.acceleration.x * 5f, 0f);	
+	}
+}
