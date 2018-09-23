61ecd6b5d47beaac52f368059aa25440cd00249c
Gabriel <gdeperes@gmail.com>
2018-09-23 00:27:09 -0300

    Balacing and fixed boss movemnet

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour {
+	[SerializeField] private float bossSpeed;
+	private Rigidbody2D myRigidbody2d;
+	void Start () {
+		myRigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 v = Camera.main.WorldToViewportPoint(transform.position + Input.acceleration);
+		if (v.x > 0f && v.x < 1f)
+			myRigidbody2d.velocity = new Vector2(Input.acceleration.x * bossSpeed, 0f);	
+		else
+			myRigidbody2d.velocity = new Vector2(0f, 0f);	
+	}
+}
